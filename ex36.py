64e08e67577465f54fd62f0f7ce8f4182ce85ae3
moninger <moninger76131@gmail.com>
2021-01-18 17:12:03 +0100

    игра про суса

--- ex36.py
+++ ex36.py
@@ -1,7 +1,7 @@
 #       вш1                                 вш2
 #        |                                   |
 # __________________________________________________
-# (_________________________________________________)
+# (_________________________________монтер__________)
 #      ()
 #      |              ___________            | Д
 #      |_________()  |бункер     |           | Е
@@ -25,13 +25,94 @@
 # 
 import time
 from sys import exit
+
 def dead():
     print("Тебя поймали монтеры.")
     exit(0)
 
 def bunker():
-    print("Тут сус, боно, биг бро. Да вообще все")
+    print("Тут Котофей, биг бро. Да вообще все")
+    exit(0)
 
 def luk():
     print("Полезем в люк?")
     print("\n(1)Да\t(2)Нет")
+    luk_open = False
+    while True:
+        answer = input("> ")
+        
+        if answer == "да":
+            print("Люк открыт. Боно, открывай гирмодверь!")
+            bunker()
+
+
+        elif answer == "нет" and not luk_open:
+
+            print("Попробуем вскрыть люк?")
+            luk_open = True
+            
+            answer = input("> ")
+            if answer == "да":
+                bunker()
+            else:
+                dead()
+
+def tunnel1():
+    print("Ты в тоннеле. монтер в другом конце тоннеля. Побежать к люку?")
+    print("\n(1)Да\t(2)Нет")
+
+    answer = input("> ")
+    if answer == "да":
+        print("ты убежал от монтеров. Лезем в люк?")
+        print("\n(1)Да\t(2)Нет")
+
+        answer = input("> ")
+        if answer == "да":
+            luk()
+        elif answer == "нет":
+            dead()
+
+    elif answer == "нет":
+        dead()
+
+def vent3():
+    print("Лезем в эту шахту?")
+    answer = input("> ")
+
+    if answer == "да":
+        print("Ты в тоннеле")
+        tunnel1()
+    elif answer == "нет":
+        dead()
+
+def vent4():
+    print("Лезем в эту шахту?")
+    answer = input("> ")
+
+    if answer == "да":
+        print("Ты в тоннеле, но тут монтер")
+        dead()
+    elif answer == "нет":
+        dead()
+
+def tunnel2():
+
+def vent2():
+    print("Лезем в эту шахту?")
+    answer = input("> ")
+
+    if answer == "да":
+        print("Ты в тоннеле, но тут монтер")
+        dead()
+    elif answer == "нет":
+        dead()
+
+def vent1():
+    print("Лезем в эту шахту?")
+    answer = input("> ")
+
+    if answer == "да":
+        print("Ты в тоннеле")
+        tunnel2()
+    elif answer == "нет":
+        dead()
