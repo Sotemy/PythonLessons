1a4fb017fe8466704c45ed2d20b84f275a83faf0
moninger <moninger76131@gmail.com>
2021-05-26 18:48:47 +0200

    ver 1.0

--- ex42.py
+++ ex42.py
@@ -1,12 +1,15 @@
 # animal nasleduet object
 class Animal(object):
-    pass
+
+    def jump(self):
+        print("I have jumped")
 
 # Dog nasleduet Animal
 class Dog(Animal):
     def __init__(self, name):
 
         self.name = name
+        print(f"{name} is here")
 
 
 class Cat(Animal):
@@ -14,6 +17,7 @@ class Cat(Animal):
     def __init__(self, name):
 
         self.name = name
+        print(f"{name} is here")
 
 class Person(object):
 
@@ -26,5 +30,26 @@ class Person(object):
 
 class Employee(Person):
     def __init__(self, name, salary):
-
+        # ssilka na person
         super(Employee, self).__init__(name)
+        self.salary = salary
+
+class Fish(object):
+    pass
+
+class Salmon(Fish):
+    pass
+
+class Halibut(Fish):
+    pass
+
+barbos = Dog("Barbos")
+
+barsik = Cat("Barsik")
+
+mary = Person("Mary")
+
+mary.pet = barsik
+
+print(barbos.name)
+barbos.jump()
